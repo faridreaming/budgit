61ccca5b1f5a511281580321dc9b1016d872edc4
Muhammad Farid Yamin <97384053+faridreaming@users.noreply.github.com>
2025-05-20 06:13:16 +0700

    feat: add tambah penghasilan

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,144 @@
+import { nanoid } from 'nanoid'
 import Dashboard from './components/Dashboard'
 import Transaksi from './components/Transaksi'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+// 0: Pengeluaran
+// 1: Penghasilan
+const kategori = [
+  {
+    id: 1,
+    nama: 'Hobi',
+    jenis: 1,
+    icon: '⚽',
+  },
+  {
+    id: 2,
+    nama: 'Makanan',
+    jenis: 0,
+    icon: '🍔',
+  },
+  {
+    id: 3,
+    nama: 'Transportasi',
+    jenis: 0,
+    icon: '🚗',
+  },
+  {
+    id: 4,
+    nama: 'Pendidikan',
+    jenis: 0,
+    icon: '🎓',
+  },
+  {
+    id: 5,
+    nama: 'Kesehatan',
+    jenis: 0,
+    icon: '🏥',
+  },
+  {
+    id: 6,
+    nama: 'Pekerjaan',
+    jenis: 1,
+    icon: '💼',
+  },
+  {
+    id: 7,
+    nama: 'Kebutuhan',
+    jenis: 0,
+    icon: '🛍️',
+  },
+  {
+    id: 8,
+    nama: 'Lainnya',
+    jenis: 0,
+    icon: '💸',
+  },
+  {
+    id: 9,
+    nama: 'Lainnya',
+    jenis: 1,
+    icon: '💸',
+  },
+]
 
 export default function App() {
-  const [transaksi, setTransaksi] = useState([])
-  // 0: Pengeluaran
-  // 1: Penghasilan
-  const kategori = [
+  const [transaksi, setTransaksi] = useState([
     {
-      id: 1,
-      nama: 'Hobi',
-      jenis: 1,
-      icon: '⚽',
+      id: nanoid(),
+      tanggal: new Date(),
+      jumlah: 50000,
+      keterangan: 'Saldo Awal',
+      idKategori: 9,
+      deskripsi: 'Saldo Awal',
     },
-    {
-      id: 2,
-      nama: 'Makanan',
-      jenis: 0,
-      icon: '🍔',
-    },
-    {
-      id: 3,
-      nama: 'Transportasi',
-      jenis: 0,
-      icon: '🚗',
-    },
-    {
-      id: 4,
-      nama: 'Pendidikan',
-      jenis: 0,
-      icon: '🎓',
-    },
-    {
-      id: 5,
-      nama: 'Kesehatan',
-      jenis: 0,
-      icon: '🏥',
-    },
-    {
-      id: 6,
-      nama: 'Pekerjaan',
-      jenis: 1,
-      icon: '💼',
-    },
-    {
-      id: 7,
-      nama: 'Kebutuhan',
-      jenis: 0,
-      icon: '🛍️',
-    },
-  ]
+  ])
 
-  const getSisaSaldo = () => {
-    return 100000
-  }
+  const [sisaSaldo, setSisaSaldo] = useState(50000)
+  const [totalPengeluaran, setTotalPengeluaran] = useState(0)
+  const [totalPenghasilan, setTotalPenghasilan] = useState(50000)
+  const [totalTransaksi, setTotalTransaksi] = useState(50000)
 
-  const getTotalPengeluaran = () => {
-    return 100000
-  }
+  useEffect(() => {
+    const savedTransaksi = JSON.parse(localStorage.getItem('transaksi'))
+    const savedSisaSaldo = JSON.parse(localStorage.getItem('sisaSaldo'))
+    const savedTotalPengeluaran = JSON.parse(localStorage.getItem('totalPengeluaran'))
+    const savedTotalPenghasilan = JSON.parse(localStorage.getItem('totalPenghasilan'))
+    const savedTotalTransaksi = JSON.parse(localStorage.getItem('totalTransaksi'))
 
-  const getTotalPenghasilan = () => {
-    return 100000
-  }
+    if (savedTransaksi && savedTransaksi.length > 0) {
+      setTransaksi(savedTransaksi)
+    }
+    if (savedSisaSaldo) {
+      setSisaSaldo(savedSisaSaldo)
+    }
+    if (savedTotalPengeluaran) {
+      setTotalPengeluaran(savedTotalPengeluaran)
+    }
+    if (savedTotalPenghasilan) {
+      setTotalPenghasilan(savedTotalPenghasilan)
+    }
+    if (savedTotalTransaksi) {
+      setTotalTransaksi(savedTotalTransaksi)
+    }
+  }, [])
 
-  const getTotalTransaksi = () => {
-    return 100000
-  }
+  useEffect(() => {
+    if (transaksi.length > 0) {
+      localStorage.setItem('transaksi', JSON.stringify(transaksi))
+    }
+  }, [transaksi])
 
   const onAddPengeluaran = (newPengeluaran) => {
     setTransaksi([...transaksi, newPengeluaran])
-    console.log(transaksi)
-    console.log(transaksi)
+    setSisaSaldo(sisaSaldo - newPengeluaran.jumlah)
+    setTotalPengeluaran(totalPengeluaran + newPengeluaran.jumlah)
+    setTotalTransaksi(totalTransaksi + newPengeluaran.jumlah)
+  }
+
+  const onAddPenghasilan = (newPenghasilan) => {
+    setTransaksi([...transaksi, newPenghasilan])
+    setSisaSaldo(sisaSaldo + newPenghasilan.jumlah)
+    setTotalPenghasilan(totalPenghasilan + newPenghasilan.jumlah)
+    setTotalTransaksi(totalTransaksi + newPenghasilan.jumlah)
   }
 
   return (
-    <div className="container mx-auto h-screen max-w-md p-4">
+    <div className="container mx-auto h-screen max-w-lg p-4">
       <h1 className="text-center text-2xl font-bold">💰 BUDGIT</h1>
       <p className="text-center">Catatan Keuangan Pribadi</p>
       <Dashboard
-        sisaSaldo={getSisaSaldo()}
-        totalPengeluaran={getTotalPengeluaran()}
-        totalPenghasilan={getTotalPenghasilan()}
-        totalTransaksi={getTotalTransaksi()}
+        sisaSaldo={sisaSaldo}
+        totalPengeluaran={totalPengeluaran}
+        totalPenghasilan={totalPenghasilan}
+        totalTransaksi={totalTransaksi}
       />
-      <Transaksi transaksi={transaksi} kategori={kategori} onAddPengeluaran={onAddPengeluaran} />
+      <Transaksi
+        transaksi={transaksi}
+        kategori={kategori}
+        onAddPengeluaran={onAddPengeluaran}
+        onAddPenghasilan={onAddPenghasilan}
+      />
     </div>
   )
 }
