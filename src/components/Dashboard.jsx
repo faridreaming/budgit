1a99df403b776f7c1f87bb00fee7fdfc555ba329
Muhammad Farid Yamin <97384053+faridreaming@users.noreply.github.com>
2025-05-19 22:12:03 +0700

    feat: add dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react'
+import { Eye, EyeOff } from 'lucide-react'
+import ToggleTheme from './ToggleTheme'
+
+export default function Dashboard({
+  sisaSaldo,
+  totalPengeluaran,
+  totalPenghasilan,
+  totalTransaksi,
+}) {
+  const [isSaldoVisible, setIsSaldoVisible] = useState(() => {
+    return localStorage.getItem('isSaldoVisible') === 'true'
+  })
+
+  const getPesanWaktu = () => {
+    const waktu = new Date()
+    const jam = waktu.getHours()
+    if (jam >= 0 && jam < 12) {
+      return 'pagi! 🌄'
+    } else if (jam >= 12 && jam < 15) {
+      return 'siang! ☀️'
+    } else if (jam >= 15 && jam < 18) {
+      return 'sore! 🌅'
+    } else {
+      return 'malam! 🌙'
+    }
+  }
+
+  const toggleSaldoVisibility = () => {
+    setIsSaldoVisible((prev) => !prev)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('isSaldoVisible', isSaldoVisible)
+  }, [isSaldoVisible])
+
+  return (
+    <div className="bg-base-100 rounded-box relative mt-4 p-4">
+      <h2 className="text-lg font-bold">Halo 👋 Selamat {getPesanWaktu()}</h2>
+      <div className="mt-2 flex items-center gap-2">
+        <span>Saldo:</span>
+        <span id="sisa-saldo">{isSaldoVisible ? `Rp${sisaSaldo}` : '********'}</span>
+        <label className="swap">
+          <input type="checkbox" checked={isSaldoVisible} onChange={toggleSaldoVisibility} />
+          <div className="swap-on">
+            <Eye className="size-4" />
+          </div>
+          <div className="swap-off">
+            <EyeOff className="size-4" />
+          </div>
+        </label>
+      </div>
+      <div className="mt-2 grid grid-cols-1 gap-2 text-center sm:grid-cols-2">
+        <div className="bg-error/8 text-error rounded-field p-4">
+          <h2>Total Pengeluaran</h2>
+          <p>Rp{totalPengeluaran}</p>
+        </div>
+        <div className="bg-success/8 text-success rounded-field p-4">
+          <h2>Total Penghasilan</h2>
+          <p>Rp{totalPenghasilan}</p>
+        </div>
+        <div className="bg-warning/8 text-warning rounded-field p-4 sm:col-span-2">
+          <h2>Kamu sudah melakukan transaksi sebanyak</h2>
+          <p>Rp{totalTransaksi}</p>
+        </div>
+      </div>
+      <ToggleTheme />
+    </div>
+  )
+}
