87f0d510ef2e30af6c1bd7f5051e25a91a9dd558
Muhammad Farid Yamin <97384053+faridreaming@users.noreply.github.com>
2025-05-23 21:53:25 +0700

    style(eslint): add an ignored file

--- eslint.config.js
+++ eslint.config.js
@@ -8,11 +8,7 @@ import prettier from 'eslint-config-prettier'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      prettier,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,12 +20,15 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       quotes: ['error', 'single'],
       semi: ['error', 'never'],
+    },
+  },
+  {
+    files: ['src/components/**/*.{ts,tsx,js,jsx}'],
+    rules: {
+      quotes: 'off',
     },
   }
 )
