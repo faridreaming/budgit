953bc22e7db979abc35bb37601dce3dc221f03ec
Muhammad Farid Yamin <97384053+faridreaming@users.noreply.github.com>
2025-06-27 23:49:29 +0700

    feat: readd `initialFocus` attribute to the calendar

--- src/components/TambahTransaksi.tsx
+++ src/components/TambahTransaksi.tsx
@@ -215,7 +215,7 @@ export default function TambahTransaksi({
                           </FormControl>
                         </PopoverTrigger>
                         <PopoverContent className="w-auto p-0">
-                          <Calendar mode="single" selected={field.value} onSelect={field.onChange} />
+                          <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
                         </PopoverContent>
                       </Popover>
                     </div>
